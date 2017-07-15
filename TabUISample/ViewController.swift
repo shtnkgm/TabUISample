611c045010fdb4214e291e85e7cbc2de0ab6bbdc
shtnkgm <nakagami.shota@gmail.com>
2017-07-15 23:56:09 +0900

    Initial Commit

---
+++ TabUISample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabUISample
+//
+//  Created by Shota Nakagami on 2017/07/15.
+//  Copyright © 2017年 Shota Nakagami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
