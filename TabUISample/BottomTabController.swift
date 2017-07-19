c2fa34230e54004338ae26bbc1d1e34ac2434861
shtnkgm <nakagami.shota@gmail.com>
2017-07-20 01:09:43 +0900

    clean

--- TabUISample/BottomTabController.swift
+++ TabUISample/BottomTabController.swift
@@ -21,56 +21,10 @@ class BottomTabController: UITabBarController {
 
     /// 表示するタブを初期化する
     private func setupTabs() {
-        let viewControllers = [createViewControllerInFirstTab(),
-                               createViewControllerInSecondTab(),
-                               createViewControllerInThirdTab(),
-                               createViewControllerInFourthTab()]
+        let viewControllers = [ViewControllerFactory.createFeed(),
+                               ViewControllerFactory.createSearch(),
+                               ViewControllerFactory.createFavorite(),
+                               ViewControllerFactory.createSettings()]
         setViewControllers(viewControllers, animated: false)
     }
-
-    /// 1番目のタブのViewControllerを生成する
-    private func createViewControllerInFirstTab() -> UIViewController {
-        let viewController = TopTabController.create()
-        viewController.tabBarItem = UITabBarItem(title: "フィード",
-                                                  image: UIImage(named: "feed"),
-                                                  selectedImage: UIImage(named: "feed_filled"))
-
-        let dataSource = (1...5).map {
-            (number: Int) -> TopTabItem in
-            let title = "フィード" + "\(number)"
-            let viewController = CustomViewController.create()
-            viewController.title = title
-            return TopTabItem(title: title, viewController: viewController)
-        }
-        viewController.configure(dataSource: dataSource)
-        return viewController
-    }
-
-    /// 2番目のタブのViewControllerを生成する
-    private func createViewControllerInSecondTab() -> UIViewController {
-        let viewController = CustomViewController.create()
-        viewController.tabBarItem = UITabBarItem(title: "探す",
-                                                  image: UIImage(named: "search"),
-                                                  selectedImage: UIImage(named: "search_filled"))
-        return viewController
-    }
-
-    /// 3番目のタブのViewControllerを生成する
-    private func createViewControllerInThirdTab() -> UIViewController {
-        let viewController = CustomViewController.create()
-        viewController.tabBarItem = UITabBarItem(title: "お気に入り",
-                                                 image: UIImage(named: "favorite"),
-                                                 selectedImage: UIImage(named: "favorite_filled"))
-        return viewController
-    }
-
-    /// 4番目のタブのViewControllerを生成する
-    private func createViewControllerInFourthTab() -> UIViewController {
-        let viewController = CustomViewController.create()
-        viewController.tabBarItem = UITabBarItem(title: "設定",
-                                                 image: UIImage(named: "settings"),
-                                                 selectedImage: UIImage(named: "settings_filled"))
-        return viewController
-    }
-
 }
---
+++ TabUISample/ViewControllerFactory.swift
@@ -0,0 +1,57 @@
+//
+//  ViewControllerFactory.swift
+//  TabUISample
+//
+//  Created by Shota Nakagami on 2017/07/20.
+//  Copyright © 2017年 Shota Nakagami. All rights reserved.
+//
+
+import UIKit
+
+struct ViewControllerFactory {
+
+/// 「フィード」タブのViewControllerを生成する
+static func createFeed() -> UIViewController {
+    let viewController = TopTabController.create()
+    viewController.tabBarItem = UITabBarItem(title: "フィード",
+                                             image: UIImage(named: "feed"),
+                                             selectedImage: UIImage(named: "feed_filled"))
+
+    let dataSource = (1...5).map {
+        (number: Int) -> TopTabItem in
+        let title = "フィード" + "\(number)"
+        let viewController = CustomViewController.create()
+        viewController.title = title
+        return TopTabItem(title: title, viewController: viewController)
+    }
+    viewController.configure(dataSource: dataSource)
+    return viewController
+}
+
+/// 「探す」タブのViewControllerを生成する
+static func createSearch() -> UIViewController {
+    let viewController = CustomViewController.create()
+    viewController.tabBarItem = UITabBarItem(title: "探す",
+                                             image: UIImage(named: "search"),
+                                             selectedImage: UIImage(named: "search_filled"))
+    return viewController
+}
+
+/// 「お気に入り」タブのViewControllerを生成する
+static func createFavorite() -> UIViewController {
+    let viewController = CustomViewController.create()
+    viewController.tabBarItem = UITabBarItem(title: "お気に入り",
+                                             image: UIImage(named: "favorite"),
+                                             selectedImage: UIImage(named: "favorite_filled"))
+    return viewController
+}
+
+/// 「設定」タブのViewControllerを生成する
+static func createSettings() -> UIViewController {
+    let viewController = CustomViewController.create()
+    viewController.tabBarItem = UITabBarItem(title: "設定",
+                                             image: UIImage(named: "settings"),
+                                             selectedImage: UIImage(named: "settings_filled"))
+    return viewController
+}
+}
